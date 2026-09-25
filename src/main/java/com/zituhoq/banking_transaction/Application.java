41ef2565aec3433e3d7b3459e961210e73042fb3
Zitu Hoq <zhzhitu121@gmail.com>
2026-09-25 22:06:55 +0600

    rename app and properties name

--- src/main/java/com/zituhoq/banking_transaction/Application.java
+++ src/main/java/com/zituhoq/banking_transaction/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankingTransactionApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankingTransactionApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/zituhoq/banking_transaction/TestBankingTransactionApplication.java
+++ src/test/java/com/zituhoq/banking_transaction/TestBankingTransactionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestBankingTransactionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(BankingTransactionApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
