4c08f44d9102427f6d62c1be87c727055960c3de
Zitu Hoq <zhzhitu121@gmail.com>
2026-09-26 14:23:19 +0600

    add filter to set correlation id for logging

---
+++ src/main/java/com/zituhoq/banking_transaction/filters/CorrelationIdFilter.java
@@ -0,0 +1,27 @@
+package com.zituhoq.banking_transaction.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.MDC;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class CorrelationIdFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpReq = (HttpServletRequest) req;
+        String correlationId = httpReq.getHeader("X-Correlation-Id");
+
+        if(correlationId == null || correlationId.isEmpty()) {
+            correlationId = UUID.randomUUID().toString();
+        }
+        MDC.put("correlationId", correlationId);
+        try{
+            chain.doFilter(req,res);
+        }finally {
+            MDC.clear();
+        }
+    }
+}
